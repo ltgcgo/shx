9a78ad392c431d13cd66ab5f3651207ff3165b2e
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-03-15 19:42:07 +0000

    Now with Docker support.

--- sh/sh.sh
+++ sh/sh.sh
@@ -6,13 +6,21 @@ if [ -f "nix/${useNix}.nix" ]; then
 else
 	echo "${useNix}.nix does not exist."
 fi
-if [ -e "$(which nix-shell)" ]; then
+if [ -e "$(which nix-shells)" ]; then
 	nix-shell nix/${useNix}.nix --quiet --pure --command zsh
 	echo "Quitting Nix shell..."
 	rm nix/.zcompdump 2> /dev/null
 	rm nix/zsh/.zcompdump 2> /dev/null
 elif [ -e "$(which podman)" ]; then
-	podman run -ti --rm nixpkgs/nix-unstable
+	if [ ! -f "nix/zsh/.docker_name" ]; then
+		dd if=/dev/random bs=3 count=4 | basenc --base64url > nix/zsh/.docker_name
+	fi
+	ociName="$(cat nix/zsh/.docker_name)"
+	echo "Starting Podman..."
+	podman run -it -d --name "$ociName" -v "$SOURCE_DIR":/data docker.io/nixpkgs/nix sleep infinity 2>/dev/null
+	podman start "$ociName" 2>/dev/null
+	podman exec -it "$ociName" bash /data/nix/zsh/boot.sh "$useNix"
+	echo "Quitting Podman..."
 else
 	echo "Nix Shell is not available."
 fi
