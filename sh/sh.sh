0c149339537c325a7c44a80bf8742320cfb60edd
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-03-15 18:46:50 +0000

    Prepare for Docker/Podman-based containers.

--- sh/sh.sh
+++ sh/sh.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 # shx Standard Utility
+useNix=${1:-env}
+if [ -f "nix/${useNix}.nix" ]; then
+	echo "Preparing Nix shell with: ${useNix}.nix..."
+else
+	echo "${useNix}.nix does not exist."
+fi
 if [ -e "$(which nix-shell)" ]; then
-	useNix=${1:-env}
-	if [ -f "nix/${useNix}.nix" ]; then
-		echo "Preparing Nix shell with: ${useNix}.nix..."
-		nix-shell nix/${useNix}.nix --quiet --pure --command zsh
-		echo "Quitting Nix shell..."
-		rm nix/.zcompdump 2> /dev/null
-		rm nix/zsh/.zcompdump 2> /dev/null
-	else
-		echo "${useNix}.nix does not exist."
-	fi
+	nix-shell nix/${useNix}.nix --quiet --pure --command zsh
+	echo "Quitting Nix shell..."
+	rm nix/.zcompdump 2> /dev/null
+	rm nix/zsh/.zcompdump 2> /dev/null
+elif [ -e "$(which podman)" ]; then
+	podman run -ti --rm nixpkgs/nix-unstable
 else
 	echo "Nix Shell is not available."
 fi
