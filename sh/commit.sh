fd503d44a3aa2c37d5b79fb9a248e4c586a82089
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-04-02 07:37:54 +0000

    Clean before committing.

--- sh/commit.sh
+++ sh/commit.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 #shx Standard Utility
+# Remove useless files
+tree -iafl --gitignore | grep "\.swp$" | while IFS= read -r file; do
+	if [ -f "$file" ]; then
+		rm -v "$file"
+	fi
+done
 # Push to a single remote
 git stage -A && git commit && git push $1
 # Push to all remotes
